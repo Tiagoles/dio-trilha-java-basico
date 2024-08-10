0f41b77713d5ceb76792e648e23a8f5aca50434d
tiago damasceno <tiagodamascenogames@gmail.com>
2024-08-10 11:34:12 -0300

    Declarando tipo de variaveis

--- estudos/src/edu/dio/estudos/TipoVariaveis/TipoVariaveis.java
+++ estudos/src/edu/dio/estudos/TipoVariaveis/TipoVariaveis.java
@@ -2,6 +2,11 @@ package edu.dio.estudos.TipoVariaveis;
 
 public class TipoVariaveis {
     public static void main(String[] args) {
-        System.out.println("OLA mundo");
+        final double PI = 3.14;
+        String meuNome = "Tiago";
+        short numeroCurto = 1;
+        int numeroInteiro = 32;
+        float numeroFloat = 32.5F;
+        System.out.println(PI + " " + meuNome);
     }
 }
